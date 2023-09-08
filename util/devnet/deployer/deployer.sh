5ca90015f0a81ed611275accf34bf03ba95fc69f
Morgan Bazalgette <morgan@morganbaz.com>
2023-09-08 18:36:57 +0200

    feat: add docker config & set up

---
+++ util/devnet/deployer/deployer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Auto-deploys realm on any change.
+
+t1mnemonic="source bonus chronic canvas draft south burst lottery vacant surface solve popular case indicate oppose farm nothing bullet exhibit title speed wink action roast"
+export SHELL="/bin/bash"
+
+if [ ! -f ".deployer.touch" ]; then
+	printf '\n\n%s\n\n' "$t1mnemonic" | gnokey add --recover --insecure-password-stdin test1
+	touch .deployer.touch
+fi
+
+cd realm/
+
+sleep 7.5 # get gno node set up
+
+ls *.gno | entr -ns '
+path=gno.land/r/demo/chess_$RANDOM
+echo "publishing as $path"
+echo | gnokey maketx addpkg \
+	--gas-wanted 10000000 \
+	--gas-fee 1ugnot \
+	--pkgpath $path \
+	--pkgdir . \
+	--insecure-password-stdin \
+	--remote gnoland:26657 \
+	--broadcast test1'
