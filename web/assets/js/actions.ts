6a469d1c149afc64d85d99e8313fa99cab6b32f2
Morgan Bazalgette <morgan@morganbaz.com>
2023-09-21 18:22:03 +0200

    add claimtimeout

--- web/assets/js/actions.ts
+++ web/assets/js/actions.ts
@@ -243,7 +243,7 @@ class Actions {
   public async getGame(gameID: string): Promise<Game> {
     const gameResponse: string = (await this.provider?.evaluateExpression(
       chessRealm,
-      `GetGame(${gameID})`
+      `GetGame("${gameID}")`
     )) as string;
 
     // Parse the response
@@ -493,6 +493,35 @@ class Actions {
     return JSON.parse(acceptResponseRaw);
   }
 
+  /**
+   * Claim that a timeout has occurred on the given game.
+   * @param gameID the ID of the running game
+   */
+  async claimTimeout(gameID: string): Promise<Game> {
+    // Make the request
+    const response: BroadcastTxCommitResult = (await this.wallet?.callMethod(
+      chessRealm,
+      'ClaimTimeout',
+      [gameID],
+      TransactionEndpoint.BROADCAST_TX_COMMIT,
+      undefined,
+      {
+        gasFee: defaultTxFee,
+        gasWanted: defaultGasWanted
+      }
+    )) as BroadcastTxCommitResult;
+
+    // Parse the response from the node
+    const claimTimeoutRaw: string | null =
+      response.deliver_tx.ResponseBase.Data;
+    if (!claimTimeoutRaw) {
+      throw new Error('invalid draw refuse response');
+    }
+
+    // Magically parse the response
+    return JSON.parse(claimTimeoutRaw);
+  }
+
   /****************
    * DASHBOARD
    ****************/
@@ -530,7 +559,7 @@ class Actions {
   async getPlayer(playerID: string): Promise<Player> {
     const playerResponse: string = (await this.provider?.evaluateExpression(
       chessRealm,
-      `GetPlayer(${playerID})`
+      `GetPlayer("${playerID}")`
     )) as string;
 
     // Parse the response
