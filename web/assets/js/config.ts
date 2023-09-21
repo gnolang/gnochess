86c5a466625ba81e8077dfeb4ae4e5817201b0d9
alexiscolin <alexis@jaunebleu.co>
2023-09-21 05:20:28 +0200

    reenable env file

--- web/assets/js/config.ts
+++ web/assets/js/config.ts
@@ -1,17 +1,17 @@
 export const { VITE_GNO_WS_URL, VITE_GNO_CHESS_REALM, VITE_FAUCET_URL } =
   import.meta.env;
 
-// if (!VITE_GNO_WS_URL) {
-//   throw Error('VITE_GNO_WS_URL property not found in .env');
-// }
+if (!VITE_GNO_WS_URL) {
+  throw Error('VITE_GNO_WS_URL property not found in .env');
+}
 
-// if (!VITE_GNO_CHESS_REALM) {
-//   throw Error('VITE_GNO_CHESS_REALM property not found in .env');
-// }
+if (!VITE_GNO_CHESS_REALM) {
+  throw Error('VITE_GNO_CHESS_REALM property not found in .env');
+}
 
-// if (!VITE_FAUCET_URL) {
-//   throw Error('VITE_FAUCET_URL property not found in .env');
-// }
+if (!VITE_FAUCET_URL) {
+  throw Error('VITE_FAUCET_URL property not found in .env');
+}
 
 export default {
   GNO_WS_URL: VITE_GNO_WS_URL,
