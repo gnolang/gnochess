578ac6f76be16b668c835329d43d29950efc8adc
Albert Le Batteux <contact@albttx.tech>
2023-09-22 13:33:10 +0100

    fix: change api url

--- signup/src/services/rest/rest.ts
+++ signup/src/services/rest/rest.ts
@@ -7,7 +7,11 @@ const baseUrl = Config.API_BASE_URL;
 export class RestService {
   static async post<T>(params: RequestParams) {
     return axios
-      .post<T>(`${baseUrl}/api/${params.url}`, params.data, params.config)
+      .post<T>(
+        `${window.location.protocol}//${baseUrl}/api/${params.url}`,
+        params.data,
+        params.config
+      )
       .then((response) => response.data);
   }
 }
