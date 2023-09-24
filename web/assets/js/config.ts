0a632cf12c1f2f14e43382e4452daf58621cbe32
Milos Zivkovic <milos.zivkovic@tendermint.com>
2023-09-25 00:57:23 +0200

    Make ENV consistent

--- web/assets/js/config.ts
+++ web/assets/js/config.ts
@@ -1,6 +1,6 @@
 export const {
   VITE_GNO_WS_URL,
-  VITE_JSONRPC_URL,
+  VITE_GNO_JSONRPC_URL,
   VITE_GNO_CHESS_REALM,
   VITE_FAUCET_URL
 } = import.meta.env;
@@ -9,8 +9,8 @@ if (!VITE_GNO_WS_URL) {
   throw Error('VITE_GNO_WS_URL property not found in .env');
 }
 
-if (!VITE_JSONRPC_URL) {
-  throw Error('VITE_JSONRPC_URL property not found in .env');
+if (!VITE_GNO_JSONRPC_URL) {
+  throw Error('VITE_GNO_JSONRPC_URL property not found in .env');
 }
 
 if (!VITE_GNO_CHESS_REALM) {
@@ -23,7 +23,7 @@ if (!VITE_FAUCET_URL) {
 
 export default {
   GNO_WS_URL: VITE_GNO_WS_URL,
-  GNO_JSONRPC_URL: VITE_JSONRPC_URL,
+  GNO_JSONRPC_URL: VITE_GNO_JSONRPC_URL,
   GNO_CHESS_REALM: VITE_GNO_CHESS_REALM,
   FAUCET_URL: VITE_FAUCET_URL
 };
