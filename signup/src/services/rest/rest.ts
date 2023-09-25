44f0519c7a7bab17dacf6a865b078d7ac8cf3219
Albert Le Batteux <contact@albttx.tech>
2023-09-25 13:05:07 +0100

    chore: setup simple dev-env

--- signup/src/services/rest/rest.ts
+++ signup/src/services/rest/rest.ts
@@ -6,12 +6,13 @@ const baseUrl = Config.API_BASE_URL;
 
 export class RestService {
   static async post<T>(params: RequestParams) {
+    let url = `${window.location.protocol}//${baseUrl}/api/${params.url}`;
+    if (Config.NETLIFY_FUNCTIONS_URL !== '') {
+      url = `${Config.NETLIFY_FUNCTIONS_URL}/.netlify/functions/${params.url}`;
+    }
+
     return axios
-      .post<T>(
-        `${window.location.protocol}//${baseUrl}/api/${params.url}`,
-        params.data,
-        params.config
-      )
+      .post<T>(url, params.data, params.config)
       .then((response) => response.data);
   }
 }
